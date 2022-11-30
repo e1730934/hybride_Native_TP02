462ea73ea95dfee3eac0e4dfce64c0d7942bf28d
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-11-30 15:51:07 -0500

    refactored getPodcasts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,6 @@ export default function Home(){
             if (res.ok) {
                 const data = await res.json();
                 setPodcast(data);
-                setPodcastFiltrer(data);
             } else {
                 console.log("Error Podcasts not loaded");
             }
